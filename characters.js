f01dbb90e31ce4683629ab8907e8a5b88bd4ad5d
Berta Esteve <berta.esteve.escoda@estudiantat.upc.edu>
2026-03-16 23:16:01 +0100

    Limpieza de archivos antiguos e implementación de la clase Character

---
+++ characters.js
@@ -0,0 +1,30 @@
+export class Character {
+  constructor(id, name, status, species, image, origin) {
+    this.id = id;
+    this.name = name;
+    this.status = status;
+    this.species = species;
+    this.image = image;
+    this.originName = origin.name;
+  }
+
+  get description() {
+    return `${this.name} (${this.species} - ${this.status})`;
+  }
+
+}
+
+export const loadCharacters = async (n) => {
+  
+  const response = await fetch(`https://rickandmortyapi.com/api/character`);
+  const { results } = await response.json();
+  
+  const characters = [];
+
+  for (const { id, name, status, species, gender, origin, image } of results) {
+    characters.push(new Character(id, name, status, species, gender, origin, image));
+  }
+  
+  return characters;
+};
+
